2014cc49fa5a8b298fc59b619f8b10c619e27863
Rafael <rflernesto@gmail.com>
2023-01-14 18:22:08 -0400

    chore: create avl tree basic public moperations

--- src/main/kotlin/dataStructures/tree/avl/AvlTree.kt
+++ src/main/kotlin/dataStructures/tree/avl/AvlTree.kt
@@ -1,4 +1,17 @@
 package dataStructures.tree.avl
 
 class AvlTree {
+    private val data = mutableListOf<Int>()
+
+    fun add(element: Int) {
+
+    }
+
+    fun remove(element: Int) {
+
+    }
+
+    fun print(){
+        
+    }
 }

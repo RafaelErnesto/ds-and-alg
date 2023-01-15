35f5e3b10d464e08a5b8b7aa477a840a57d97c92
Rafael <rflernesto@gmail.com>
2023-01-15 19:05:23 -0400

    feat: add to avl tree

--- src/main/kotlin/dataStructures/tree/avl/AvlTree.kt
+++ src/main/kotlin/dataStructures/tree/avl/AvlTree.kt
@@ -1,10 +1,35 @@
 package dataStructures.tree.avl
 
+data class Node(
+    var value: Int,
+    var left: Node? = null,
+    var right: Node? = null
+)
 class AvlTree {
-    private val data = mutableListOf<Int>()
+    private var root: Node? = null
 
     fun add(element: Int) {
+        if(root == null) {
+            root = Node(element)
+            return
+        }
 
+        var currentNode = root
+        while(currentNode != null) {
+            if(element < currentNode.value) {
+                if(currentNode.left != null) {
+                    currentNode = currentNode.left
+                } else {
+                    currentNode.left  = Node(element)
+                    return
+                }
+            } else  if(currentNode.right != null) {
+                currentNode = currentNode.right
+            } else {
+                currentNode.right  = Node(element)
+                return
+            }
+        }
     }
 
     fun remove(element: Int) {
@@ -12,6 +37,6 @@ class AvlTree {
     }
 
     fun print(){
-        
+
     }
 }
---
+++ src/main/kotlin/dataStructures/tree/avl/AvlTreeMain.kt
@@ -0,0 +1,8 @@
+package dataStructures.tree.avl
+
+fun main(){
+    val avl = AvlTree()
+    for (i in 1..10){
+        avl.add(i)
+    }
+}

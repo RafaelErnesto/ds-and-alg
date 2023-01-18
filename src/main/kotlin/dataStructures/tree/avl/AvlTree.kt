7fb6880e3f74825fd365a08c1c61c2cd6c411434
Rafael <rflernesto@gmail.com>
2023-01-17 21:48:12 -0400

    feat: add counter for node height

--- src/main/kotlin/dataStructures/tree/avl/AvlTree.kt
+++ src/main/kotlin/dataStructures/tree/avl/AvlTree.kt
@@ -18,7 +18,6 @@ class AvlTree {
         if(element <= node!!.value){
             if(node.left == null){
                 node.left = Node(element)
-                return
             } else {
                 add(element, node.left)
             }
@@ -27,11 +26,17 @@ class AvlTree {
         if(element > node!!.value) {
             if(node.right == null){
                 node.right = Node(element)
-                return
             } else {
                 add(element, node.right)
             }
         }
+        node.height = calculateHeight(node)
+    }
+
+    private fun calculateHeight(node: Node): Int{
+        val leftSubTreeHeight: Int = if(node.left != null) node.left!!.height else 0
+        val rightSubTreeHeight: Int = if(node.right != null) node.right!!.height else 0
+        return if (leftSubTreeHeight > rightSubTreeHeight) leftSubTreeHeight + 1 else rightSubTreeHeight + 1
     }
 
     fun remove(element: Int) {
@@ -40,7 +45,7 @@ class AvlTree {
 
     fun printPreOrderTree(node: Node? = root){
         if(node == null) return
-        println(node.value)
+        println("Node: " + node.value +" height: "+node.height)
         printPreOrderTree(node.left)
         printPreOrderTree(node.right)
     }
